e8864e38e3b7747710e500f7f05bc132c42c563e
Mark Slorach <hello@markslorach.com>
2024-07-08 14:10:08 +0100

    updated footer

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 const Footer = () => {
   return (
     <footer className="h-20 items-center flex">
-        <p>created by <Link className="text-blue-400" href="https://www.markslorach.com/">mark slorach</Link>.</p>
+        <p>created by <Link className="text-blue-400" href="https://www.markslorach.com/" target="_blank">mark slorach</Link>.</p>
     </footer>
   )
 }
