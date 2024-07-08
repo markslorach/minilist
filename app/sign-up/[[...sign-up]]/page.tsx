2907f58b716b7e1428de2677ca0fab4f7b542c69
Mark Slorach <hello@markslorach.com>
2024-07-08 16:54:08 +0100

    updated sign in / out modal styling

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="flex min-h-[80vh] justify-center items-center">
-      <SignUp />;
+      <SignUp />
     </div>
   );
 }
