23086a93ce29fdb899ab824b3fe753db5f32b277
Mark Slorach <hello@markslorach.com>
2024-07-09 15:37:44 +0100

    useRef to clean form input on submit

--- app/AddTaskForm.tsx
+++ app/AddTaskForm.tsx
@@ -1,24 +1,34 @@
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { CirclePlus } from "lucide-react"
-import { createTask } from "./actions/taskActions"
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { CirclePlus } from "lucide-react";
+import { createTask } from "./actions/taskActions";
+import { useRef } from "react";
 
 const AddTaskForm = () => {
-  return (
-    <form action={createTask}>
-    <div className="flex space-x-2">
-      <Input
-        type="text"
-        name="task"
-        placeholder="What do you want to do?"
-        className="border-none bg-transparent dark:bg-transparent px-0"
-      />
-      <Button className="bg-transparent dark:bg-transparent hover:bg-transparent dark:hover:bg-transparent text-gray-500 dark:text-gray-400 p-0">
-        <CirclePlus />
-      </Button>
-    </div>
-  </form>
-  )
-}
+  const ref = useRef<HTMLFormElement>(null);
 
-export default AddTaskForm
+  return (
+    <form
+      ref={ref}
+      action={async (formData) => {
+        ref.current?.reset();
+        await createTask(formData);
+      }}
+    >
+      <div className="flex space-x-2">
+        <Input
+          type="text"
+          name="task"
+          placeholder="What do you want to do?"
+          className="border-none bg-transparent dark:bg-transparent px-0"
+        />
+        <Button className="bg-transparent dark:bg-transparent hover:bg-transparent dark:hover:bg-transparent text-gray-500 dark:text-gray-400 p-0">
+          <CirclePlus />
+        </Button>
+      </div>
+    </form>
+  );
+};
+
+export default AddTaskForm;
