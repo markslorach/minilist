1af6435c60aff7af84496364e43c4101e40f2961
Mark Slorach <hello@markslorach.com>
2024-07-09 17:05:45 +0100

    added source code link to footer

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link"
+import { Github } from 'lucide-react';
 
 const Footer = () => {
   return (
-    <footer className="h-20 items-center flex">
+    <footer className="h-20 items-center flex justify-between">
         <p>created by <Link className="text-blue-500" href="https://www.markslorach.com/" target="_blank">mark slorach</Link>.</p>
+        <Link href="https://github.com/markslorach/minilist.git" target="_blank"><Github className="w-5 h-5"/></Link>
     </footer>
   )
 }
