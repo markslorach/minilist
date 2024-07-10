3dd1a7acb858ed045e225146ea6cd9ee8c7de12d
Mark Slorach <hello@markslorach.com>
2024-07-11 00:16:27 +0100

    conditional rendering for task information

--- app/components/tasks/TaskComponent.tsx
+++ app/components/tasks/TaskComponent.tsx
@@ -26,47 +26,50 @@ const TaskComponent = ({ tasks }: { tasks: Task[] }) => {
   return (
     <section>
       <Heading className="pt-10 pb-2">Tasks</Heading>
-      <p className="pb-10 text-sm text-gray-500 dark:text-gray-400 text-balance">
-        Click a task&apos;s checkbox to mark it as complete, or click the task
-        to view its actions.
-      </p>
-      {tasks.filter((task) => !task.completed).length < 1 ? (
-        <p className="text-sm">
-          Squeaky clean! Add some tasks above to get started.
-        </p>
-      ) : (
-        <Accordion type="single" collapsible className="w-full">
-          {tasks
-            .filter((task) => !task.completed)
-            .map((task) => (
-              <AccordionItem
-                key={task.id}
-                value={task.id.toString()}
-                className="border-none"
-              >
-                <div className="flex items-center space-x-3">
-                  <CompleteTaskForm task={task} />
-                  <div className="w-full">
-                    <AccordionTrigger>{task.title}</AccordionTrigger>
-                  </div>
+      <div className="pb-10">
+        {tasks.filter((task) => !task.completed).length < 1 ? (
+          <p className="text-sm text-gray-500 dark:text-gray-400 text-balance">
+            <i>Squeaky</i> clean! Add some tasks above to get started.
+          </p>
+        ) : (
+          <p className="text-sm text-gray-500 dark:text-gray-400 text-balance">
+            Click a task&apos;s checkbox to mark it as complete, or click the
+            task to view its actions.
+          </p>
+        )}
+      </div>
+
+      <Accordion type="single" collapsible className="w-full">
+        {tasks
+          .filter((task) => !task.completed)
+          .map((task) => (
+            <AccordionItem
+              key={task.id}
+              value={task.id.toString()}
+              className="border-none"
+            >
+              <div className="flex items-center space-x-3">
+                <CompleteTaskForm task={task} />
+                <div className="w-full">
+                  <AccordionTrigger>{task.title}</AccordionTrigger>
                 </div>
-                <AccordionContent className="h-16 flex items-center">
-                  {!updateTask ? (
-                    <div className="flex space-x-4 items-center">
-                      <Pencil
-                        onClick={() => setUpdateTask(true)}
-                        className="h-5 w-5 text-gray-600 dark:text-gray-400 cursor-pointer"
-                      />
-                      <DeleteTaskForm taskId={task.id} />
-                    </div>
-                  ) : (
-                    <UpdateTaskForm task={task} setUpdateTask={setUpdateTask} />
-                  )}
-                </AccordionContent>
-              </AccordionItem>
-            ))}
-        </Accordion>
-      )}
+              </div>
+              <AccordionContent className="h-16 flex items-center">
+                {!updateTask ? (
+                  <div className="flex space-x-4 items-center">
+                    <Pencil
+                      onClick={() => setUpdateTask(true)}
+                      className="h-5 w-5 text-gray-600 dark:text-gray-400 cursor-pointer"
+                    />
+                    <DeleteTaskForm taskId={task.id} />
+                  </div>
+                ) : (
+                  <UpdateTaskForm task={task} setUpdateTask={setUpdateTask} />
+                )}
+              </AccordionContent>
+            </AccordionItem>
+          ))}
+      </Accordion>
 
       <Heading className="pt-10 pb-2">Completed</Heading>
       <p className="pb-10 text-sm text-gray-500 dark:text-gray-400">
