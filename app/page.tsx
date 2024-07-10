a7098a190f97d1c16418cd0fd8eb09b2e69af8a9
Mark Slorach <hello@markslorach.com>
2024-07-11 00:08:16 +0100

    clerk user adding to database

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,32 @@ import { currentUser } from "@clerk/nextjs/server";
 import UserWelcome from "./UserWelcome";
 import AddTaskForm from "./components/tasks/AddTaskForm";
 import TaskComponent from "./components/tasks/TaskComponent";
+import { Prisma } from "@prisma/client";
 
 export default async function Home() {
   const user = await currentUser();
+
+  if (user) {
+    try {
+      await prisma.user.create({
+        data: {
+          email: user.emailAddresses[0].emailAddress,
+          name: user.firstName,
+        },
+      });
+    } catch (error) {
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error.code === "P2002"
+      ) {
+        console.warn("User already exists:", user.username);
+      } else {
+        console.error("Error creating user:", error);
+        throw error;
+      }
+    }
+  }
+
   const tasks = await prisma.task.findMany({
     where: {
       user: {
