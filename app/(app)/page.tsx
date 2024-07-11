e1366945c6df178b75b5c0e1738962ac75abc999
Mark Slorach <hello@markslorach.com>
2024-07-11 15:55:45 +0100

    removed email address from server log - looking into middleware solution

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -11,29 +11,30 @@ export default async function Home() {
   const user = await currentUser();
 
   if (user) {
-    const clerkEmail = user.emailAddresses[0].emailAddress;
-
+    const email = user.emailAddresses[0].emailAddress;
+  
     try {
       const existingUser = await prisma.user.findUnique({
-        where: { email: clerkEmail },
+        where: { email },
       });
-
+  
       if (!existingUser) {
         await prisma.user.create({
           data: {
-            email: clerkEmail,
+            email,
             name: user.firstName,
           },
         });
-        console.log(`New user created: ${clerkEmail}`);
+        console.log(`New user created`); 
       } else {
-        console.log(`User already exists: ${clerkEmail}`);
+        console.log(`User already exists`); 
       }
     } catch (error) {
       console.error("Error finding or creating user:", error);
-      throw error;
+      throw error; 
     }
   }
+  
 
   const tasks = await prisma.task.findMany({
     where: {
