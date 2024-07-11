122973a4ceae35b36167d0409a67feecdccd04bc
Mark Slorach <hello@markslorach.com>
2024-07-11 11:02:58 +0100

    not found 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import Heading from "./components/Heading";
+import Footer from "./Footer";
+import NavBar from "./NavBar";
+
+const NotFound = () => {
+  return (
+    <main className="flex flex-col min-h-dvh md:min-h-screen mx-auto max-w-3xl px-4 sm:px-10">
+      <NavBar />
+      <section className="flex-grow py-16">
+        <Heading className="pb-10">
+          It looks like youre lost.{" "}
+          <Link href="/" className="text-blue-500">
+            Return home
+          </Link>
+          .
+        </Heading>
+      </section>
+      <Footer />
+    </main>
+  );
+};
+
+export default NotFound;
