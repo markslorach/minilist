ce9f51bb8692f23880c7e54a839db03419dc74e3
Mark Slorach <hello@markslorach.com>
2024-07-12 11:44:40 +0100

    pending spinners for add and delete tasks

--- app/components/tasks/AddTaskButton.tsx
+++ app/components/tasks/AddTaskButton.tsx
@@ -1,16 +1,16 @@
-import { Plus } from "lucide-react";
+import { Plus, LoaderCircle } from "lucide-react";
 import { useFormStatus } from "react-dom";
 
 const AddTaskButton = () => {
   const { pending } = useFormStatus();
 
   return (
-    <button
-      type="submit"
-      aria-label="add task"
-      disabled={pending}
-    >
-      <Plus className="h-7 w-7" />
+    <button type="submit" aria-label="add task" disabled={pending}>
+      {pending ? (
+        <LoaderCircle className="animate-spin h-7 w-7" />
+      ) : (
+        <Plus className="h-7 w-7" />
+      )}
     </button>
   );
 };
--- app/components/tasks/DeleteTaskButton.tsx
+++ app/components/tasks/DeleteTaskButton.tsx
@@ -1,13 +1,16 @@
-"use client"
-import { Trash } from "lucide-react";
+"use client";
+import { Trash, LoaderCircle } from "lucide-react";
 import { useFormStatus } from "react-dom";
 
 const DeleteTaskButton = () => {
   const { pending } = useFormStatus();
   return (
     <button type="submit" aria-label="delete task" disabled={pending}>
-      {" "}
-      <Trash className="h-6 w-6 text-red-400" />
+      {pending ? (
+        <LoaderCircle className="animate-spin h-6 w-6 text-red-400" />
+      ) : (
+        <Trash className="h-6 w-6 text-red-400" />
+      )}
     </button>
   );
 };
