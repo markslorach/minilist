15462960cf0e0f34dc137815936f0654c36e99d1
Mark Slorach <hello@markslorach.com>
2024-07-15 13:39:46 +0100

    task item component

--- app/components/tasks/TaskComponent.tsx
+++ app/components/tasks/TaskComponent.tsx
@@ -1,28 +1,15 @@
 "use client";
-import { Pencil } from "lucide-react";
 import Heading from "../Heading";
 import {
   Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
 } from "@/components/ui/accordion";
-import DeleteTaskForm from "./DeleteTaskForm";
 import { Button } from "@/components/ui/button";
 import CompleteTaskForm from "./CompleteTaskForm";
-import { useState } from "react";
-import UpdateTaskForm from "./UpdateTaskForm";
 import { clearCompletedTasks } from "@/app/actions/taskActions";
-
-type Task = {
-  id: number;
-  title: string;
-  completed: boolean;
-};
+import TaskItem from "./TaskItem";
+import { Task } from "@prisma/client";
 
 const TaskComponent = ({ tasks }: { tasks: Task[] }) => {
-  const [updateTask, setUpdateTask] = useState(false);
-
   const taskComplete = tasks.filter((task) => task.completed);
   const taskPending = tasks.filter((task) => !task.completed);
 
@@ -42,6 +29,7 @@ const TaskComponent = ({ tasks }: { tasks: Task[] }) => {
   return (
     <section>
       <Heading className="pt-10 pb-2">Tasks</Heading>
+      
       <div className="pb-10">
         {taskPending.length < 1 ? (
           <p className="text-sm text-gray-500 dark:text-gray-400">
@@ -57,32 +45,7 @@ const TaskComponent = ({ tasks }: { tasks: Task[] }) => {
 
       <Accordion type="single" collapsible className="w-full">
         {taskPending.map((task) => (
-          <AccordionItem
-            key={task.id}
-            value={task.id.toString()}
-            className="border-none py-0.5"
-          >
-            <div className="flex items-center space-x-3">
-              <CompleteTaskForm task={task} />
-              <div className="w-full">
-                <AccordionTrigger>{task.title}</AccordionTrigger>
-              </div>
-            </div>
-            <AccordionContent className="h-16 flex items-center">
-              {!updateTask ? (
-                <div className="flex space-x-4 items-center">
-                  <Pencil
-                    onClick={() => setUpdateTask(true)}
-                    className="text-gray-600 dark:text-gray-400 cursor-pointer"
-                    size={25}
-                  />
-                  <DeleteTaskForm taskId={task.id} />
-                </div>
-              ) : (
-                <UpdateTaskForm task={task} setUpdateTask={setUpdateTask} />
-              )}
-            </AccordionContent>
-          </AccordionItem>
+          <TaskItem key={task.id} task={task} />
         ))}
       </Accordion>
 
---
+++ app/components/tasks/TaskItem.tsx
@@ -0,0 +1,46 @@
+import {
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import CompleteTaskForm from "./CompleteTaskForm";
+import { Pencil } from "lucide-react";
+import UpdateTaskForm from "./UpdateTaskForm";
+import DeleteTaskForm from "./DeleteTaskForm";
+import { useState } from "react";
+import { Task } from "@prisma/client";
+
+const TaskItem = ({ task }: { task: Task }) => {
+  const [updateTask, setUpdateTask] = useState(false);
+
+  return (
+    <AccordionItem
+      key={task.id}
+      value={task.id.toString()}
+      className="border-none py-0.5"
+    >
+      <div className="flex items-center space-x-3">
+        <CompleteTaskForm task={task} />
+        <div className="w-full">
+          <AccordionTrigger>{task.title}</AccordionTrigger>
+        </div>
+      </div>
+      <AccordionContent className="h-16 flex items-center">
+        {!updateTask ? (
+          <div className="flex space-x-4 items-center">
+            <Pencil
+              onClick={() => setUpdateTask(true)}
+              className="text-gray-600 dark:text-gray-400 cursor-pointer"
+              size={25}
+            />
+            <DeleteTaskForm taskId={task.id} />
+          </div>
+        ) : (
+          <UpdateTaskForm task={task} setUpdateTask={setUpdateTask} />
+        )}
+      </AccordionContent>
+    </AccordionItem>
+  );
+};
+
+export default TaskItem;
