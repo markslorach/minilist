998b2dbc95f1079ee931b142fcfede5e4d46fba7
Mark Slorach <hello@markslorach.com>
2024-08-18 21:46:12 +0100

    404 not found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,19 +5,15 @@ import NavBar from "./components/shared/NavBar";
 
 const NotFound = () => {
   return (
-    <main className="mx-auto flex min-h-dvh max-w-3xl flex-col px-4 sm:px-10 md:min-h-screen">
-      <NavBar />
-      <section className="flex-grow py-16">
-        <Heading className="pb-10">
-          It looks like you&apos;re lost.{" "}
-          <Link href="/" className="text-blue-500">
-            Return home
-          </Link>
-          .
-        </Heading>
-      </section>
-      <Footer />
-    </main>
+    <div className="py-16">
+      <Heading className="pb-10">
+        It looks like you&apos;re lost.{" "}
+        <Link href="/" className="text-blue-500">
+          Return home
+        </Link>
+        .
+      </Heading>
+    </div>
   );
 };
 
